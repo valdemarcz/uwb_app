967585fa6457164a5a590bcbda9a1859ad847bc2
Valdemar Černiavski <32278772+valdemarcz@users.noreply.github.com>
2025-11-19 21:54:02 +0200

    Add database connection setup in db.php

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$host = getenv('DB_HOST'); // e.g., 127.0.0.1 if using proxy
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+
+$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+]);
+
+echo "Connected successfully!";
